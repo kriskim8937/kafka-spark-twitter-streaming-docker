29a68c25f20281b56a78f08275970d52452dd9e6
kriskim <kriskim8937@gmail.com>
2021-10-25 01:16:58 +0900

    [pc] Correct the topic of consumer

--- consumer/src/main/java/com/kris/kafka/myPac/IKafkaConstants.java
+++ consumer/src/main/java/com/kris/kafka/myPac/IKafkaConstants.java
@@ -13,7 +13,7 @@ public interface IKafkaConstants {
 
     public static String GROUP_ID_CONFIG="consumerGroup1";
 
-    public static Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
+    public static Integer MAX_NO_MESSAGE_FOUND_COUNT=1000;
 
     public static String OFFSET_RESET_LATEST="latest";
 
--- producer/src/main/java/com/kris/kafka/ProducerMain.java
+++ producer/src/main/java/com/kris/kafka/ProducerMain.java
@@ -6,12 +6,15 @@ import java.net.URISyntaxException;
 import com.kris.kafka.myPac.*;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProducerMain {
     public static void main(String[] args) throws IOException, URISyntaxException {
         runProducer();
     }
     static void runProducer() throws IOException, URISyntaxException {
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
         Producer<String, String> producer = ProducerCreator.createProducer();
         TwitterHandler twitterHandler = new TwitterHandler();
         BufferedReader reader = null;
@@ -20,7 +23,7 @@ public class ProducerMain {
         int key = 0;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("try_Buffered3Part", Integer.toString(key), line);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(IKafkaConstants.TOPIC_NAME, Integer.toString(key), line);
             key++;
             System.out.println(key);
             producer.send(producerRecord);
